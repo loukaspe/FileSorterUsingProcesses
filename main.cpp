935cf6b134e8866d4985cfb5af045966d1f39bac
loukaswhatdup <loukaspe20@gmail.com>
2019-11-16 16:44:26 +0200

    Added Main Program (v1)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
+#include "Coordinator.h"
 
-const int RETURN_FAILURE = 1;
-const int RETURN_SUCCESS = 0;
+void sorter(int sorterNumber, int coachNumber) {
+    cout << "I am sorter no. " << sorterNumber + 1 << " of Coach no. " << coachNumber + 1 << endl;
+}
+
+void createSorters(int sortersToBeCreated, int coachNumber) {
+    for(int i = 0; i < sortersToBeCreated; i++) {
+        pid_t id = fork();
+
+        if(id == 0){
+            sorter(i, coachNumber);
+            exit(0);
+        }
+        wait(NULL);
+    }
+}
+
+void coach(int coachNumber) {
+    int sortersToBeCreated = pow(2, coachNumber);
+    createSorters(sortersToBeCreated, coachNumber);
+
+}
 
 int main(int argc, char** argv) {
+    const long BUFFER_SIZE = 1024;
+    char* filename = argv[1];
+    int numberOfCoaches = 4;
+    RecordReader* recordReader = new RecordReader(filename);
+    MyRecord* records = recordReader->fetchAllRecords();
 
-    if ( argc != 2 ) {
-        printf("Correct syntax is: %s BinaryFile\n", argv[0]);
-        return(RETURN_FAILURE);
-    }
+    Coordinator* coordinator = new Coordinator(
+        filename,
+        records,
+        BUFFER_SIZE,
+        numberOfCoaches
+    );
+    coordinator->doAction();
 
-    return RETURN_SUCCESS;
+    return 0;
 }
